23ab8078d8b757f8b69811634e80a5980cb4e6d1
michelle <miranny.c@gmail.com>
2021-11-05 20:04:49 -0300

    continuação dos exs

---
+++ Fundamentos/bloco-04-Introducao-a-JS-e-logica-de-programacao/retorno.js
@@ -0,0 +1,9 @@
+const valorDefinido = 50;
+
+if ( valorDefinido < 0){
+    console.log("negativo");
+}else if (valorDefinido > 0){
+    console.log("positivo");
+} else{
+    console.log("zero");
+}   
