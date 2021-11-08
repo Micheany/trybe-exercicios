af9c5b780a1c7e9fbfeaeff8c0cc17c6e1ced1a9
michelle <miranny.c@gmail.com>
2021-11-08 18:57:32 -0300

    maior numero do arry com for

--- Fundamentos/bloco-04-Introducao-a-JS-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio4.js
+++ Fundamentos/bloco-04-Introducao-a-JS-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio4.js
@@ -1,4 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-numbers.sort();
-console.log("O maior número contido na array é " + numbers[0]);
+let maior = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    
+    if(numbers[index] > maior){
+        maior = numbers[index]
+    }
+    
+}
+console.log( "O maior numero do array é: " + maior);
